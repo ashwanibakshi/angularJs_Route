273333175c324f6cf3f05ca018de357d82b1a660
Ashwani Bakshi <ab324002@gmail.com>
2021-01-27 22:46:39 +0530

    first commit

---
+++ app/appRoute.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="E:\XAMARIN\2021\angularjsdemo2\angularjsdemo2\scripts/angular.min.js" />
+/// <reference path="controller/demoController.js" />
+
+var appRoute = angular.module('Route', ["ngRoute"]);
+
+appRoute.config(['$routeProvider','$locationProvider',function ($routeProvider,$locationProvider) {
+    $routeProvider.when("/first", {
+        templateUrl: "/app/views/FrontOne.html",
+        controller:"myController"
+    }).when("/",{
+        templateUrl: "/app/views/HomePage.html",
+        controller:"myController"
+    }).otherwise({
+        redirectTo: "/"
+    })
+
+}]);
---
+++ app/controller/demoController.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="E:\XAMARIN\2021\angularjsdemo2\angularjsdemo2\scripts/angular.min.js" />
+/// <reference path="E:\XAMARIN\2021\angularjsdemo2\angularjsdemo2\app/appRoute.js" />
+
+var myapp = angular.module('myapp', []);
+
+appRoute.controller('myController', function ($scope,$location) {
+    $scope.message = "This is page one"
+    $scope.home ="we are on home page"
+
+    $scope.redirect= function(){
+        $location.path('/');
+    }
+});
